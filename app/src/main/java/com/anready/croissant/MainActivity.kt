6a9d64d049cf81f826592010c5ddf170727f4e3f
Anready <nfc939@gmail.com>
2024-08-01 14:12:28 +0300

    Added FileProvider

--- app/src/main/java/com/anready/croissant/MainActivity.kt
+++ app/src/main/java/com/anready/croissant/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.anready.croissant
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Environment
+import android.provider.Settings
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var responseTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        responseTextView = findViewById(R.id.textView)
+        responseTextView.text = if (checkPermission()) { "Permission granted" } else { "No permission" }
+
+        if (!checkPermission()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            } else {
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    101)
+            }
+            return
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Environment.isExternalStorageManager()
+        } else {
+            ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
         }
     }
 }
---
+++ app/src/main/java/com/anready/croissant/providers/Files.kt
@@ -0,0 +1,105 @@
+package com.anready.croissant.providers
+
+import android.Manifest
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.net.Uri
+import android.database.MatrixCursor
+import android.os.Build
+import android.os.Environment
+import androidx.core.content.ContextCompat
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.File
+
+class Files : ContentProvider() {
+    override fun query(
+        uri: Uri,
+        projection: Array<String>?,
+        selection: String?,
+        selectionArgs: Array<String>?,
+        sortOrder: String?,
+    ): Cursor {
+        val cursor = MatrixCursor(arrayOf("response"))
+        val path = uri.getQueryParameter("path")
+        val command = uri.getQueryParameter("command")
+
+        if (!checkPermission()) {
+            cursor.addRow(arrayOf(message("error", "ERR_01: No permission to access external storage")))
+            return cursor
+        }
+
+        when (command) {
+            "list" -> {
+                cursor.addRow(arrayOf(listObjects(path.toString())))
+            }
+            "isPermissionsGranted" -> {
+                cursor.addRow(arrayOf(message("result", checkPermission())))
+            }
+            else -> {
+                cursor.addRow(arrayOf(message("error", "ERR_03: Unknown command")))
+            }
+        }
+
+        return cursor
+    }
+
+    private fun message(type: String, message: Any): String {
+        val error = JSONObject()
+        error.put(type, message)
+        return JSONArray().put(error).toString()
+    }
+
+    private fun checkPermission(): Boolean {
+        val context = context ?: return false
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Environment.isExternalStorageManager()
+        } else {
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    private fun listObjects(path: String): String {
+        val files = File(Environment.getExternalStorageDirectory().absolutePath + path).listFiles()
+        val filesArray = JSONArray()
+
+        if (files == null || path.isEmpty()) {
+            return message("error", "ERR_02: No path provided")
+        }
+
+        for (file in files) {
+            val fileObject = JSONObject()
+            fileObject.put("name", file.name)
+            fileObject.put("type", file.isDirectory)
+            fileObject.put("visibility", file.isHidden)
+            filesArray.put(fileObject)
+        }
+
+        return filesArray.toString()
+    }
+
+    override fun onCreate(): Boolean {
+        return true
+    }
+
+    override fun getType(uri: Uri): String? {
+        return null
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        return null
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+        return 0
+    }
+
+    override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<String>?): Int {
+        return 0
+    }
+}
