2d8595b8959bc5afe1d509541b078c7366f84fd6
Anready <nfc939@gmail.com>
2024-09-17 17:04:13 +0300

    Make logs a little bit beautify

---
+++ app/src/main/java/com/anready/croissant/providers/Log.kt
@@ -0,0 +1,3 @@
+package com.anready.croissant.providers
+
+class Log (val date: String, val app: String, val action: String)
--- app/src/main/java/com/anready/croissant/providers/LogsActivity.kt
+++ app/src/main/java/com/anready/croissant/providers/LogsActivity.kt
@@ -2,13 +2,16 @@ package com.anready.croissant.providers
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.anready.croissant.Constants.LOGS
 import com.anready.croissant.R
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class LogsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,14 +38,51 @@ class LogsActivity : AppCompatActivity() {
 
     private fun showLogs() {
         val sharedPreferences = getSharedPreferences(LOGS, Context.MODE_PRIVATE)
-        val logsTv = findViewById<android.widget.TextView>(R.id.logsTv)
-        // leave 1 line after each log
-        logsTv.text = sharedPreferences.all.toString().replace(", ", ",\n\n")
+        val logsLv = findViewById<android.widget.ListView>(R.id.logsLv)
+
+        val logsMap = sharedPreferences.all
+        val logsList = ArrayList<Log>()
+        val logsPreview = ArrayList<String>()
+
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+        val sortedKeys = logsMap.keys.sortedByDescending { key ->
+            val datePart = key.substring(0, 19)
+            dateFormat.parse(datePart)
+        }
+
+        for (key in sortedKeys) {
+            val value = logsMap[key]
+            logsList.add(Log(key.substring(0, 19), key.substring(20, key.thirdIndexOf(" ")), value.toString()))
+            logsPreview.add("$key: $value")
+        }
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, logsPreview)
+        logsLv.adapter = adapter
+
+        logsLv.setOnItemClickListener { _, _, position, _ ->
+            val selectedLog = logsList[position]
+            val alertDialog = AlertDialog.Builder(this)
+            alertDialog.setTitle(selectedLog.date)
+            alertDialog.setMessage("App ${selectedLog.app}\nAction: ${selectedLog.action}")
+            alertDialog.setPositiveButton("ОК") { dialog, _ ->
+                dialog.dismiss()
+            }
+            alertDialog.show()
+        }
     }
+
 
     private fun clearLogs() {
         val sharedPreferences = getSharedPreferences(LOGS, Context.MODE_PRIVATE)
         sharedPreferences.edit().clear().apply()
         showLogs()
+    }
+}
+
+private fun String.thirdIndexOf(s: String): Int {
+    return if (this.indexOf(s, this.indexOf(s, this.indexOf(s) + 1) + 1) != -1) {
+        this.indexOf(s, this.indexOf(s, this.indexOf(s) + 1) + 1)
+    } else {
+        this.length
     }
 }
